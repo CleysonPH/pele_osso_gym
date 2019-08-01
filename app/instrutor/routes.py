3e6317d2d6d0529f4ac94323eeefabad54d2cb24
Cleyson Lima <cleysonph@gmail.com>
2019-08-01 01:34:29 -0300

    Implementei a rota e o templete para cadastro de Instrutores

--- app/instrutor/routes.py
+++ app/instrutor/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
 
-from app import app
+from app import app, db
 from app.instrutor.models import Instrutor
 
 
@@ -11,3 +11,31 @@ def listar_instrutores():
 
     return render_template(
         'instrutor/listar_instrutores.html', titulo=titulo, instrutores=instrutores)
+
+
+@app.route('/cadastar/instrutor/', methods=['GET', 'POST'])
+def cadastrar_instrutor():
+    titulo = 'Cadastrar Instrutor'
+    instrutor = None;
+
+    if request.method == 'GET':
+        return render_template('instrutor/formulario_instrutor.html', titulo=titulo, instrutor=instrutor)
+
+    instrutor = Instrutor(
+        nome = request.form.get('nome'),
+        email = request.form.get('email'),
+        cpf = request.form.get('cpf'),
+        telefone = request.form.get('telefone'),
+        data_nascimento = request.form.get('data_nascimento'),
+        sexo = request.form.get('sexo'),
+        faculdade = request.form.get('faculdade'),
+        confef_cref = request.form.get('confef_cref'),
+        status = 'A',
+    )
+
+    db.session.add(instrutor)
+    db.session.commit()
+
+    flash('Instrutor cadastrado com sucesso!')
+
+    return redirect(url_for('cadastrar_instrutor'))
