4d7d8fe6c348b625dd0525f7e8805500481b93da
Cleyson Lima <cleysonph@gmail.com>
2019-08-04 23:56:04 -0300

    Implementei a edição do Treino

--- app/treino/routes.py
+++ app/treino/routes.py
@@ -29,6 +29,30 @@ def cadastrar_treino(id):
     db.session.add(treino)
     db.session.commit()
 
-    flash('Treino Cadastrado com sucesso!')
+    flash('Treino cadastrado com sucesso!')
 
     return redirect(url_for('detalhar_ficha', id=id))
+
+
+@app.route('/editar/treino/<int:id>/', methods=['GET', 'POST'])
+def editar_treino(id):
+    treino = Treino.query.get_or_404(id)
+    exercicios = Exercicio.query.filter(Exercicio.status == 'A')
+    
+    titulo = 'Editar Treino'
+
+    if request.method == 'GET':
+        return render_template('treino/formulario_treino.html', titulo=titulo, treino=treino, exercicios=exercicios)
+    
+    
+    treino.exercicio=Exercicio.query.get(request.form.get('exercicio'))
+    treino.carga=request.form.get('carga')
+    treino.repeticao=request.form.get('repeticao')
+    treino.secao=request.form.get('secao')
+    treino.descanso=request.form.get('descanso')
+
+    db.session.commit()
+
+    flash('Treino editado com sucesso!')
+
+    return redirect(url_for('detalhar_ficha', id=treino.ficha.id))
