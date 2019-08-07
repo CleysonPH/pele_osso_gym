ce52a1a373793bdf17ea07f3c02ede9150ce81c3
Cleyson Lima <cleysonph@gmail.com>
2019-08-06 22:37:40 -0300

    Implementei a validação de CPF no formulario do Aluno

--- app/aluno/forms.py
+++ app/aluno/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, FloatField, SelectField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, DateField, FloatField, SelectField, TextAreaField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, Optional
 
 
 class AlunoForm(FlaskForm):
@@ -9,11 +9,20 @@ class AlunoForm(FlaskForm):
     telefone = StringField('Telefone para contato', validators=[DataRequired()])
     cpf = StringField('CPF', validators=[DataRequired()])
     data_nascimento = DateField('Data de Nascimento', validators=[DataRequired()], format='%d/%m/%Y')
-    peso = FloatField('Peso', validators=[DataRequired()])
-    altura = FloatField('Altura', validators=[DataRequired()])
+    peso = FloatField('Peso', validators=[Optional()])
+    altura = FloatField('Altura', validators=[Optional()])
     sexo = SelectField(
         'Sexo',
         choices=[('F', 'Feminino'), ('M', 'Masculino'), ('X', 'Prefere não informar')]
     )
-    observacoes = TextAreaField('Observações', validators=[DataRequired()])
+    observacoes = TextAreaField('Observações', validators=[Optional()])
     submit = SubmitField('Salvar')
+
+
+    def validate_cpf(form, field):
+        from validate_docbr import CPF
+
+        cpf = CPF()
+
+        if not cpf.validate(field.data):
+            raise ValidationError("CPF Invalido")
